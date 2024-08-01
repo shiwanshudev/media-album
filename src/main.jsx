d9a3c7f4a972d22b6f2843e561ba9cddbc83e06d
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-08-01 09:52:55 +0530

    Add dependencies and create store

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import React from "react";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const el = document.querySelector("#root");
 createRoot(el).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
