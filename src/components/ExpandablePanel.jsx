62cf5f237675d5afc5eef34f118ec2a51c7cc2d4
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-08-09 12:43:33 +0530

    Add albums expandable panel

---
+++ src/components/ExpandablePanel.jsx
@@ -0,0 +1,20 @@
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+import { useState } from "react";
+
+export default function ExpandablePanel({ children, header }) {
+  const [expanded, setExpanded] = useState(false);
+  const handleClick = () => setExpanded(!expanded);
+  return (
+    <div className="mb-2 border rounded">
+      <div className="flex p-2 justify-between items-center">
+        <div className="flex flex-row items-center justify-between">
+          {header}
+        </div>
+        <div onClick={handleClick} className="cursor-pointer">
+          {expanded ? <GoChevronDown /> : <GoChevronLeft />}
+        </div>
+      </div>
+      {expanded && <div>{children}</div>}
+    </div>
+  );
+}
--- src/components/UsersListItem.jsx
+++ src/components/UsersListItem.jsx
@@ -2,23 +2,26 @@ import { removeUser } from "../store";
 import Button from "./Button";
 import { GoTrashcan } from "react-icons/go";
 import { useThunk } from "../hooks/use-thunk";
+import ExpandablePanel from "./ExpandablePanel";
+import AlbumsList from "./AlbumsList";
 
 export default function UsersListItem({ user }) {
   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
   const handleDelete = () => {
     doRemoveUser(user);
   };
+  const header = (
+    <>
+      <Button className="mr-3" loading={isLoading} onClick={handleDelete}>
+        <GoTrashcan />
+      </Button>
+      {error && "Error..."}
+      {user.name}
+    </>
+  );
   return (
-    <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center justify-between">
-          <Button className="mr-3" loading={isLoading} onClick={handleDelete}>
-            <GoTrashcan />
-          </Button>
-          {error && "Error..."}
-          {user.name}
-        </div>
-      </div>
-    </div>
+    <ExpandablePanel header={header}>
+      <AlbumsList user={user} />
+    </ExpandablePanel>
   );
 }
