33ddc14c7d8d337f72a13a16799e2308eefbdb0d
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-08-10 20:54:39 +0530

    Fix rtk query api bug

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,3 +1,7 @@
+import { useFetchAlbumsQuery } from "../store/apis/albumsApi";
+
 export default function AlbumsList({ user }) {
+  const { data, isLoading } = useFetchAlbumsQuery(user);
+  console.log(data, isLoading);
   return <div>Albums for {user.name}</div>;
 }
--- src/components/ExpandablePanel.jsx
+++ src/components/ExpandablePanel.jsx
@@ -1,12 +1,10 @@
 import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 import { useState } from "react";
-import { useFetchAlbumsQuery } from "../store/apis/albumsApi";
 
 export default function ExpandablePanel({ children, header }) {
   const [expanded, setExpanded] = useState(false);
   const handleClick = () => setExpanded(!expanded);
-  const data = useFetchAlbumsQuery("6f7b");
-  console.log(data);
+
   return (
     <div className="mb-2 border rounded">
       <div className="flex p-2 justify-between items-center">
