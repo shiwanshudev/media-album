4fac7ad2b7d86a6cd0fcabbbce5c3d929dec467e
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-08-11 13:00:03 +0530

    Add album list for albums

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,7 +1,28 @@
 import { useFetchAlbumsQuery } from "../store/apis/albumsApi";
+import ExpandablePanel from "./ExpandablePanel";
+import Skeleton from "./Skeleton";
 
 export default function AlbumsList({ user }) {
-  const { data, isLoading } = useFetchAlbumsQuery(user);
-  console.log(data, isLoading);
-  return <div>Albums for {user.name}</div>;
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = <div>Error loading albums...</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos in the album...
+        </ExpandablePanel>
+      );
+    });
+  }
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 }
